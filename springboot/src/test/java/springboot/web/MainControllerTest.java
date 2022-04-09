619eac5c351edeed41a2660605425adc527c0787
DDAMDOO <sjl9524@naver.com>
2022-04-09 18:05:11 +0900

    테스트코드 생성 및 간단한 get방식 페이지 설정

---
+++ springboot/src/test/java/springboot/web/MainControllerTest.java
@@ -0,0 +1,11 @@
+package springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainControllerTest {
+    @Test
+    void contextLoads() {
+    }
+}
